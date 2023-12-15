b51a67a1fcb3f3298189637555c3f963f239567a
trsoo24 <trsoo24@naver.com>
2023-12-16 02:17:54 +0900

    프로젝트 생성

---
+++ src/main/java/SKT/demo/KakaoTalkDdaRaHaGiApplication.java
@@ -0,0 +1,12 @@
+package SKT.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoTalkDdaRaHaGiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTalkDdaRaHaGiApplication.class, args);
+	}
+}
